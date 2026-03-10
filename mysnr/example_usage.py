d02e19df405600187a60f7ade2a120974dea2d30
Kilian David <kilian.david@tum.de>
2026-03-10 13:43:12 +0100

    updates example code

--- mysnr/example_usage.py
+++ mysnr/example_usage.py
@@ -8,9 +8,10 @@ plt.rcParams['lines.marker'] = 'x'
 plt.rcParams['axes.grid'] = True
 
 
-N = int(1E6)    # total number of samples
-T_samp = 100E-9 # sampling interval
-f = 3.0E3       # fundamental signal frequency
+T_samp = 100E-9         # sampling interval
+f = 3.0E3               # fundamental signal frequency
+k = 5                   # number of periods in timeframe
+N = int(k/(f * T_samp)) # total number of samples
 
 signal_power = 0.5
 harmonic_power = 0.045
@@ -21,11 +22,14 @@ y = np.sqrt(signal_power * 2) * np.sin(f * 2.0 * np.pi *t)                  # ba
 y += np.sqrt(harmonic_power * 2) * np.sin(2*f * 3.0 * np.pi *t)             # third harmonic, power = 0.5 * 0.3**2 = 0.045
 y += np.random.normal(0, np.sqrt(noise_power), len(t))                      # add white noise over the full band
 
-py_xf, py_yf = periodogram(x=y, fs=1/T_samp, window='hann', scaling='density')
+py_xf, py_yf = periodogram(x=y, fs=1/T_samp, window='boxcar', scaling='density')
 
-harmonic_bins = mysnr.findBinnedHarmonics(py_yf, py_xf, f, 0.5/T_samp, 3)
-signal_peak_bin = mysnr.freqToBin(py_xf, f)
-signal_bins = mysnr.getIndizesAroundPeak(py_yf, signal_peak_bin)
+#harmonic_bins = mysnr.findBinnedHarmonics(py_yf, py_xf, f, 0.5/T_samp, 3)
+#signal_peak_bin = mysnr.freqToBin(py_xf, f)
+#signal_bins = mysnr.getIndizesAroundPeak(py_yf, signal_peak_bin)
+
+snr, signal_bins = mysnr.snr(py_yf, py_xf, T_samp=T_samp, N=N, bw=None, return_bins=True)
+sndr, signal_bins, harmonic_bins = mysnr.sndr(py_yf, py_xf, T_samp=T_samp, N=N, bw=None, return_bins=True)
 
 print(f"Expected Signal Power: {signal_power}")
 print(f"Calculated Signal power: {mysnr.bandpower(py_yf[signal_bins], tau=T_samp*N)}")
@@ -34,17 +38,17 @@ print(f"Expected Harm Power: {harmonic_power}")
 print(f"Calculated Harmonic Power: {mysnr.bandpower(py_yf[harmonic_bins], tau=T_samp*N)}")
 
 print(f"Expected SNR: {10*np.log10(signal_power/noise_power)}")
-print(f"Calculated SNR: {10*np.log10(mysnr.snr(py_yf, py_xf, T_samp=T_samp, N=N, bw=None))}")
+print(f"Calculated SNR: {10*np.log10(snr)}")
 
 print(f"Expected SNDR: {10*np.log10(signal_power/(noise_power + harmonic_power))}")
-print(f"Calculated SNDR: {10*np.log10(mysnr.sndr(py_yf, py_xf, T_samp=T_samp, N=N, bw=None))}")
+print(f"Calculated SNDR: {10*np.log10(sndr)}")
 
 fig, fftax = plt.subplots()
 py_yfdb = 10*np.log10(np.abs(py_yf))
 
 fftax.semilogx(py_xf, py_yfdb)
-fftax.semilogx(py_xf[harmonic_bins], py_yfdb[harmonic_bins])
-fftax.semilogx(py_xf[signal_bins], py_yfdb[signal_bins])
+fftax.semilogx(py_xf[harmonic_bins], py_yfdb[harmonic_bins], linestyle='')
+fftax.semilogx(py_xf[signal_bins], py_yfdb[signal_bins], linestyle='')
 
 plt.grid(which='minor', linestyle=':', linewidth=0.5, color='lightgray')  # Minor grid lines
 plt.show()
